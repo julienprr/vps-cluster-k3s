1bb9dbce75c0d9c76e914c72d44e1d9d1b24778d
Julien Perrier <perrierjulien22@gmail.com>
2025-06-27 17:31:01 +0200

    update elk-prod.sh

--- scripts/deploy/elk-prod.sh
+++ scripts/deploy/elk-prod.sh
@@ -4,24 +4,24 @@ echo "Création du namespace 'logging'..."
 kubectl apply -f infrastructure/namespaces/logging.yaml
 
 echo "Déploiement du certificat TLS pour kibana.julienprr.com..."
-kubectl apply -f certificate.yaml
+kubectl apply -f apps/elk/certificate.yaml
 
 echo "Déploiement de l'ingress Kibana..."
-kubectl apply -f ingress-kibana.yaml
+kubectl apply -f apps/elk/ingress-kibana.yaml
 
 echo "Installation d'Elasticsearch via Helm..."
 helm upgrade --install elasticsearch elastic/elasticsearch \
   -n logging \
-  -f elasticsearch-values.yaml
+  -f apps/elk/elasticsearch-values.yaml
 
 echo "Installation de Kibana via Helm..."
 helm upgrade --install kibana elastic/kibana \
   -n logging \
-  -f kibana-values.yaml
+  -f apps/elk/kibana-values.yaml
 
 echo "Installation de Filebeat via Helm..."
 helm upgrade --install filebeat elastic/filebeat \
   -n logging \
-  -f filebeat-values.yaml
+  -f apps/elk/filebeat-values.yaml
 
 echo "Stack ELK déployée avec succès dans l'environnement PROD."
