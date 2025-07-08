bdbd7cfcb72a53064f48c68106606ec60a0f47b5
Julien Perrier <perrierjulien22@gmail.com>
2025-07-08 16:33:30 +0200

    Fix route for play-manager-backend scripts

--- scripts/deploy/play-manager-backend-dev.sh
+++ scripts/deploy/play-manager-backend-dev.sh
@@ -9,12 +9,12 @@ echo "[PLAY MANAGER BACKEND DEV] Création du namespace 'play-manager'..."
 kubectl apply -f "$SCRIPT_DIR/../../infrastructure/namespaces/dev.yaml"
 
 echo "[PLAY MANAGER BACKEND DEV] Déploiement du certificat TLS..."
-kubectl apply -f "$SCRIPT_DIR/../../apps/play-manager/backend/dev/certificate.yaml"
+kubectl apply -f "$SCRIPT_DIR/../../apps/play-manager-backend/dev/certificate.yaml"
 
 echo "[PLAY MANAGER BACKEND DEV] Déploiement de l'ingress..."
-kubectl apply -f "$SCRIPT_DIR/../../apps/play-manager/backend/dev/ingress.yaml"
+kubectl apply -f "$SCRIPT_DIR/../../apps/play-manager-backend/dev/ingress.yaml"
 
 echo "[PLAY MANAGER BACKEND DEV] Déploiement des ressources Kubernetes..."
-kubectl apply -f "$SCRIPT_DIR/../../apps/play-manager/backend/dev/"
+kubectl apply -f "$SCRIPT_DIR/../../apps/play-manager-backend/dev/"
 
 echo "[PLAY MANAGER BACKEND DEV] Déploiement terminé."
