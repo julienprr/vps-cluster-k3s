28153539e39bb566fe41381dd2be881fe9974193
Julien Perrier <perrierjulien22@gmail.com>
2025-07-14 18:01:10 +0200

    fix typo elk-prod.sh

--- scripts/deploy/elk-prod.sh
+++ scripts/deploy/elk-prod.sh
@@ -9,13 +9,13 @@ kubectl apply -f "$SCRIPT_DIR/../../infrastructure/namespaces/logging.yaml"
 echo "[ELK PROD] Installation de Logstash via Helm..."
 helm upgrade --install logstash $SCRIPT_DIR/../../apps/elk/logstash -n logging -f apps/elk/logstash/values.yaml
 
-echo "[ELK PROD] I‡stallation de Filebeat via Helm..."
+echo "[ELK PROD] Installation de Filebeat via Helm..."
 helm upgrade --install filebeat $SCRIPT_DIR/../../apps/elk/filebeat -n logging -f apps/elk/filebeat/values.yaml
 
-echo "[ELK PROD] Installation d'Elasticsearch via Helm..."Ò
+echo "[ELK PROD] Installation d'Elasticsearch via Helm..."
 helm upgrade --install elasticsearch $SCRIPT_DIR/../../apps/elk/elasticsearch -n logging -f apps/elk/elasticsearch/values.yaml
 
-echo "[ELK PROD] Installation de Kibana via Helm.Ò.."
+echo "[ELK PROD] Installation de Kibana via Helm..."
 helm upgrade --install kibana $SCRIPT_DIR/../../apps/elk/kibana -n logging -f apps/elk/kibana/values.yaml
 
 echo "[ELK PROD] Stack ELK déployée avec succès."
